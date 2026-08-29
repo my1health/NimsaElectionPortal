1b8211e754078ac43874c4aaa24bb97d5ae10bb6
myhealth <120328845+my1health@users.noreply.github.com>
2026-08-29 11:29:12 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
     setMessage("");
 
     try {
-      const r = await fetch("/api/vote", {
+      const r = await fetch("/api/payment/initialize", {
         method: "POST",
 
         headers: {
