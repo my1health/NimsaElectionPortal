49350f64a468694031c0547017d4edd8abb67b48
myhealth <120328845+my1health@users.noreply.github.com>
2026-09-10 19:20:59 +0100

    Update page.tsxaaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1026,7 +1026,7 @@ NAVBAR
 
       <div className="footer-bottom">
 
-         <p>© 2026<a href= "https://www.profitableratecpmnetwork.com/qh3wyrdh3?key=dfae89b620f59ba95a2079ea69b6f688">NiMSA • Asclepius Awards</a></p>
+         <p>© 2026<a href= "https://www.linkedin.com/in/bryan-daniel-174b74273">NiMSA • Asclepius Awards</a></p>
         <p>Developed by <a href= 'https://www.linkedin.com/in/bryan-daniel-174b74273'>Daniel Bryan</a></p>
 
       </div>
